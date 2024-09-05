b89cf2784e7c326b10c40e7f650a9485b98ed32f
Dominic Cork <dcork1@kent.edu>
2024-09-05 17:05:02 +0000

    Inital setup with Python and SQLite example

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 # Connect to SQLite database or create it if it doesn't exist
 conn = sqlite3.connect('example.db')
 cursor = conn.cursor()
@@ -9,18 +8,41 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS users
                     (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
 
+# Clear previous queries from the Table
+cursor.execute('DELETE FROM users')
+
 # Insert a record
-cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
+records = [
+    ('Alice', 30), 
+    ('Daniel', 27), 
+    ('Kenny', 57), 
+    ('Brock', 19), 
+    ('Steve', 45), 
+    ('Abby', 13),
+    ('Jennifer', 72),
+    ('Bob', 16),
+    ('Robby', 38), 
+    ('Anthony', 64),
+    ('Dorris', 7),
+]
+
+cursor.executemany("INSERT INTO users (name, age) VALUES (?, ?)", records)
 
 # Commit the changes
 conn.commit()
 
 # Query the database
-cursor.execute("SELECT * FROM users")
-rows = cursor.fetchall()
+cursor.execute("SELECT count(*) FROM users where age > 21")
 
-for row in rows:
-    print(row)
+num_over_21 = cursor.fetchone()[0]
+print(f"Number of users over 21: {num_over_21}")
+
+cursor.execute("SELECT name, age FROM users where age > 21 and name not like '%y' order by age desc")
+
+rows = cursor.fetchall()
+print("Users over 21 with names that do not end in 'y':")
+for name, age in rows:
+    print(f"{name}, {age}")
 
 # Close the connection
 conn.close()
